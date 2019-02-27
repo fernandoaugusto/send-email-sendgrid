022f3c930ea89cea0d689fa0fc098aca8cfec317
Fernando Augusto <fernando.augusto@inobrax.com>
2019-02-27 11:29:10 -0300

    READ.ME

--- send-email.js
+++ send-email.js
@@ -2,11 +2,6 @@ require('dotenv').config();
 
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-console.log({
-  'process.env.SENDGRID_API_KEY': process.env.SENDGRID_API_KEY,
-  'process.env.SENDGRID_API_EMAIL_TO': process.env.SENDGRID_API_EMAIL_TO,
-  'process.env.SENDGRID_API_EMAIL_FROM': process.env.SENDGRID_API_EMAIL_FROM
-});
 const msg = {
   to: process.env.SENDGRID_API_EMAIL_TO,
   from: process.env.SENDGRID_API_EMAIL_FROM,
